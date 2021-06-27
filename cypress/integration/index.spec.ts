dc6ef1c09b0f5ee623363c79105ae06f0208410a
Sungwoo Park <codest99@gmail.com>
2021-06-27 19:46:45 +0900

    test: 간단한 연산에 대한 테스트 작성

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,5 +1,119 @@
-describe("Calculator", () => {
-  it("runs basic test", () => {
+describe("Calculator module", () => {
+  const testClickResult = (fomular: string, result: string) => {
+    fomular
+      .split('')
+      .reduce<Cypress.Chainable<JQuery<HTMLElement>>>(
+        (cyObj: Cypress.Chainable<JQuery<HTMLElement>>, f: string) => (
+          cyObj
+            .get(/\d/.test(f) ? '.digit' : '.operation')
+            .contains(f)
+            .click()
+        ),
+        cy)
+      .get('#total').should('contain', result);
+  }
+
+  it("renders form of calculator", () => {
     cy.visit('/');
+    cy.get('#total').should('contain', '0');
+
+    cy.get('.calculator').contains('0');
+    cy.get('.digit').contains('1');
+    cy.get('.digit').contains('2');
+    cy.get('.digit').contains('3');
+    cy.get('.digit').contains('4');
+    cy.get('.digit').contains('5');
+    cy.get('.digit').contains('6');
+    cy.get('.digit').contains('7');
+    cy.get('.digit').contains('8');
+    cy.get('.digit').contains('9');
+
+    cy.get('.modifier').contains('AC');
+
+    cy.get('.operation').contains('+');
+    cy.get('.operation').contains('-');
+    cy.get('.operation').contains('X');
+    cy.get('.operation').contains('/');
+    cy.get('.operation').contains('=');
+  });
+
+  it("show numbers when digit button clicked", () => {
+    cy.reload();
+    testClickResult('12', '12');
+
+    cy.reload();
+    testClickResult('694', '694');
+
+    cy.reload();
+    testClickResult('000', '0');
+
+    cy.reload();
+    testClickResult('00404', '404');
+  });
+
+  it("cannot input more than 3 digits", () => {
+    cy.reload();
+    testClickResult('1234', '123');
+
+    cy.reload();
+    testClickResult('0013007', '130');
+
+    cy.reload();
+    testClickResult('0987654321', '987');
+  });
+
+  it("calculate plus operator", () => {
+    cy.reload();
+    testClickResult('1+3=', '4');
+
+    cy.reload();
+    testClickResult('1+3+5=', '9');
+
+    cy.reload();
+    testClickResult('10000+200000=', '300');
+
+    cy.reload();
+    testClickResult('10000+200000=+3211111', '621');
+  });
+
+  it("calculate minus operator", () => {
+    cy.reload();
+    testClickResult('3-1=', '2');
+
+    cy.reload();
+    testClickResult('3333333-11111111=', '222');
+
+    cy.reload();
+    testClickResult('332-99999999=', '-667');
+  });
+
+  it("calculate multiple operator", () => {
+    cy.reload();
+    testClickResult('1X2X4X4=', '32');
+
+    cy.reload();
+    testClickResult('1X2X4X0X2=', '0');
+
+    cy.reload();
+    testClickResult('123X456=', '56088');
+  });
+
+  it("calculate divide operator", () => {
+    cy.reload();
+    testClickResult('752/2=', '376');
+
+    cy.reload();
+    testClickResult('1/454231234=', '0');
+
+    cy.reload();
+    testClickResult('4632345/1234=', '3');
+  });
+
+  it("don't calculate before next operator", () => {
+    cy.reload();
+    testClickResult('1+3', '3');
+
+    cy.reload();
+    testClickResult('34*0012', '12');
   });
 });
